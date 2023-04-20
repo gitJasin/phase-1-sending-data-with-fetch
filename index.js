940ea3cbaf25669a874c1956b092d1c60c9cc784
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-20 16:42:48 -0700

    completed sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    const userInfo = {
+        name: userName,
+        email: userEmail
+    }
+    
+    const createPost = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    }
+
+    return fetch('http://localhost:3000/users', createPost)
+    .then(response => response.json())
+    .then((user) => {
+        document.querySelector('body').innerHTML = user.id
+    })
+    .catch((error) => {
+        alert('Unauthorized Access')
+        document.querySelector('body').innerHTML = error      
+    }) 
+}
+
